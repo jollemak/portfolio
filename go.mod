96679bf760f8f9bb68b21c0790a89a6aea95514c
joelmakela <joelmakela4@gmail.com>
2025-05-31 11:08:48 +0300

    fixed

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module portfolio
+module porfolio
 
-go 1.24.1
+go 1.21
--- main.go
+++ main.go
@@ -3,18 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Get port from environment variable
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
+	// Set up your static file server
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	// Your existing handlers
 	http.HandleFunc("/", HandleRoot)
 	http.HandleFunc("/portfolio", HandlePortfolio)
 
-
-fmt.Println("Server running on: http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
-	
+	fmt.Printf("Server running on port: %s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+		os.Exit(1)
+	}
 }
