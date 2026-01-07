94978eab56da3d244ab62141a3e6e88e3beccd6b
joelmakela <joelmakela4@gmail.com>
2026-01-07 11:15:42 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,15 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Portfolio",
   description: "Joel Mäkelä portfolio",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon/favicon.ico", sizes: "any" },
+    ],
+    apple: "/favicon/apple-touch-icon.png",
+  },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
